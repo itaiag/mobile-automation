9339cedfa8f0bef7843e85124e277a39d481b9cc
ranmaman <ranmaman3@gmail.com>
2012-08-07 17:40:23 -0700

    Fixed null pointer exception in case there is not config file. Commited by Itai.

--- robotium-server/src/org/topq/jsystem/mobile/RobotiumClientActivity.java
+++ robotium-server/src/org/topq/jsystem/mobile/RobotiumClientActivity.java
@@ -1,8 +1,10 @@
 package org.topq.jsystem.mobile;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+
 
 public class RobotiumClientActivity extends Activity {
 	private static final String TAG = "RobotiumClientActivity";
