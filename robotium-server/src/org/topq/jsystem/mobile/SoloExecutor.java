f669cd30f0fd9a1f728912359bb7af63397d93d8
ran_m <ran_m@top-q.co.il>
2012-09-10 10:59:06 +0300

    Implement intent.

--- robotium-server/src/org/topq/jsystem/mobile/SoloExecutor.java
+++ robotium-server/src/org/topq/jsystem/mobile/SoloExecutor.java
@@ -17,6 +17,7 @@ import org.json.JSONObject;
 
 import android.app.Instrumentation;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -29,90 +30,136 @@ public class SoloExecutor {
 	private static final String TAG = "SoloExecutor";
 	private static final String SUCCESS_STRING = "SUCCESS:";
 	private static final String ERROR_STRING = "ERROR:";
-	private static final String RESULT_STRING ="RESULT";
+	private static final String RESULT_STRING = "RESULT";
 	private Instrumentation instrumentation;
 	private Solo solo;
 	private final ISoloProvider soloProvider;
 
-	public SoloExecutor(final ISoloProvider soloProvider,Instrumentation instrumentation) {
+	public SoloExecutor(final ISoloProvider soloProvider, Instrumentation instrumentation) {
 		super();
 		this.soloProvider = soloProvider;
 		this.instrumentation = instrumentation;
 	}
 
-	public JSONObject execute(final String data) throws JSONException, IOException  {
+	public JSONObject execute(final String data) throws JSONException, IOException {
 		ScriptParser parser;
 		JSONObject result = new JSONObject();
-			parser = new ScriptParser(data);
+		parser = new ScriptParser(data);
+		Log.d(TAG, data);
 		for (CommandParser command : parser.getCommands()) {
 			if (command.getCommand().equals("enterText")) {
-				result.put(RESULT_STRING,enterText(command.getArguments()));
+				result.put(RESULT_STRING, enterText(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnButton")) {
-				result.put(RESULT_STRING,clickOnButton(command.getArguments()));
+				result.put(RESULT_STRING, clickOnButton(command.getArguments()));
 			} else if (command.getCommand().equals("launch")) {
-				result.put(RESULT_STRING,launch());
+				result.put(RESULT_STRING, launch());
 			} else if (command.getCommand().equals("clickInList")) {
-				result.put(RESULT_STRING,clickInList(command.getArguments()));
+				result.put(RESULT_STRING, clickInList(command.getArguments()));
 			} else if (command.getCommand().equals("clearEditText")) {
-				result.put(RESULT_STRING,clearEditText(command.getArguments()));
+				result.put(RESULT_STRING, clearEditText(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnButtonWithText")) {
-				result.put(RESULT_STRING,clickOnButtonWithText(command.getArguments()));
+				result.put(RESULT_STRING, clickOnButtonWithText(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnView")) {
-				result.put(RESULT_STRING,clickOnView(command.getArguments()));
+				result.put(RESULT_STRING, clickOnView(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnText")) {
-				result.put(RESULT_STRING,clickOnText(command.getArguments()));
+				result.put(RESULT_STRING, clickOnText(command.getArguments()));
 			} else if (command.getCommand().equals("sendKey")) {
-				result.put(RESULT_STRING,sendKey(command.getArguments()));
+				result.put(RESULT_STRING, sendKey(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnMenuItem")) {
-				result.put(RESULT_STRING,clickOnMenuItem(command.getArguments()));
+				result.put(RESULT_STRING, clickOnMenuItem(command.getArguments()));
 			} else if (command.getCommand().equals("getText")) {
-				result.put(RESULT_STRING,getText(command.getArguments()));
+				result.put(RESULT_STRING, getText(command.getArguments()));
 			} else if (command.getCommand().equals("getTextViewIndex")) {
-				result.put(RESULT_STRING,getTextViewIndex(command.getArguments()));
+				result.put(RESULT_STRING, getTextViewIndex(command.getArguments()));
 			} else if (command.getCommand().equals("getTextView")) {
-				result.put(RESULT_STRING,getTextView(command.getArguments()));
+				result.put(RESULT_STRING, getTextView(command.getArguments()));
 			} else if (command.getCommand().equals("getCurrentTextViews")) {
-				result.put(RESULT_STRING,getCurrentTextViews(command.getArguments()));
+				result.put(RESULT_STRING, getCurrentTextViews(command.getArguments()));
 			} else if (command.getCommand().equals("clickOnHardware")) {
-				result.put(RESULT_STRING,clickOnHardware(command.getArguments()));
-			
-			}else if(command.getCommand().equals("createFileInServer")){
-				result.put(RESULT_STRING,createFileInServer(command.getArguments()));
-			}else if(command.getCommand().equals("pull")){
+				result.put(RESULT_STRING, clickOnHardware(command.getArguments()));
+
+			} else if (command.getCommand().equals("createFileInServer")) {
+				result.put(RESULT_STRING, createFileInServer(command.getArguments()));
+			} else if (command.getCommand().equals("pull")) {
 				return pull(command.getArguments());
+			} else if (command.getCommand().equals("activateIntent")) {
+				result .put(RESULT_STRING, activateIntent(command.getArguments()));
 			}
 		}
-	
+
 		return result;
 
 	}
 
+	private String activateIntent(JSONArray arguments)  {
+		String command = null;
+		try {
+		command = "the command  activateIntent(" + arguments.getString(0) + " " + arguments.getString(1) + " " + arguments.getString(2) + " "
+				+ arguments.getString(3) + " " + arguments.getString(4) + ")";
+		
+			/*
+			 * if (arguments[0].equals("ACTION_VIEW")) { Intent webIntent = new
+			 * Intent(Intent.ACTION_VIEW, Uri.parse(arguments[1])); Log.d(TAG,
+			 * "Sending intent to " + solo.getClass().getSimpleName());
+			 * solo.getCurrentActivity().startActivityForResult(webIntent, 1);
+			 * }else if (arguments[0].equals("com.greenroad.PlayTrip")) {
+			 */
+			Log.d(TAG, "Sending intent to com.greenroad.PlayTrip");
+			Intent broadcastIntent = new Intent();
+			broadcastIntent.setAction(arguments.getString(0));
+			for (int i = 1; i < arguments.length(); i = i + 2) {
 
-	private JSONObject  pull(JSONArray arguments) throws IOException {
+				broadcastIntent.putExtra(/*
+										 * solo.getCurrentActivity().
+										 * getCallingPackage()+
+										 */arguments.getString(i), arguments.getString(i+1));
+			}
+			solo.getCurrentActivity().sendBroadcast(broadcastIntent);
+
+			// }
+			/*
+			 * else if (arguments[0].equals("ACTION_SEND")) { Intent sendIntent
+			 * = new Intent(); sendIntent.setAction(arguments[0]); for(int i =
+			 * 1;i<arguments.length;i=i+2){ Log.i(TAG, "check: " + arguments[i]
+			 * + " " + arguments[i+1]);
+			 * sendIntent.putExtra(solo.getCurrentActivity
+			 * ().getCallingPackage()+
+			 * "."+arguments[i],android.content.Intent.EXTRA_TEXT,
+			 * arguments[i+1]); }
+			 * solo.getCurrentActivity().startActivity(sendIntent); }
+			 */
+		} catch (JSONException e) {
+			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
+			Log.d(TAG, error);
+			return null;
+		}
+		return SUCCESS_STRING + command;
+	}
+
+	private JSONObject pull(JSONArray arguments) throws IOException {
 		String command = "the command  pull";
-		String allText ="";
+		String allText = "";
 		JSONObject result = null;
 		DataInputStream in = null;
 		FileInputStream fstream = null;
 		try {
-			command+="(" + arguments.getString(0)+")";
-			 fstream = new FileInputStream(arguments.getString(0));
+			command += "(" + arguments.getString(0) + ")";
+			fstream = new FileInputStream(arguments.getString(0));
 			// Get the object of DataInputStream
-			 in = new DataInputStream(fstream);
+			in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String line; 
-			while ((line = br.readLine()) != null) 
-			{     
-				allText+=line;
-			} 
-			result =  new JSONObject();
+			String line;
+			while ((line = br.readLine()) != null) {
+				allText += line;
+			}
+			result = new JSONObject();
 			result.put("file", allText);
 			in.close();
 			fstream.close();
 		} catch (IOException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
-			
+
 		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
@@ -124,20 +171,20 @@ public class SoloExecutor {
 	private String createFileInServer(JSONArray arguments) {
 		String command = "the command  createFileInServer";
 		try {
-			if(arguments.getBoolean(2)){
-				byte[] data = Base64.decode(arguments.getString(1),Base64.URL_SAFE);
-				command+="(" + arguments.getString(0) +", "+data+")";
-				command+="(" + arguments.getString(0) +", "+data+")";
+			if (arguments.getBoolean(2)) {
+				byte[] data = Base64.decode(arguments.getString(1), Base64.URL_SAFE);
+				command += "(" + arguments.getString(0) + ", " + data + ")";
+				command += "(" + arguments.getString(0) + ", " + data + ")";
 				FileOutputStream fos = new FileOutputStream(arguments.getString(0));
 				fos.write(data);
 				fos.close();
-			}else{
-				command+="(" + arguments.getString(0) +", "+arguments.getString(1)+")";
+			} else {
+				command += "(" + arguments.getString(0) + ", " + arguments.getString(1) + ")";
 				FileWriter out = new FileWriter(arguments.getString(0));
 				out.write(arguments.getString(1));
 				out.close();
 			}
-			Log.d(TAG,"run the command:"+command);
+			Log.d(TAG, "run the command:" + command);
 		} catch (IOException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
@@ -155,7 +202,7 @@ public class SoloExecutor {
 		StringBuilder response = new StringBuilder();
 		try {
 
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 
 			List<TextView> textViews = solo.getCurrentTextViews(null);
 			for (int i = 0; i < textViews.size(); i++) {
@@ -176,7 +223,7 @@ public class SoloExecutor {
 		String command = "the command  getTextView";
 		String response = "";
 		try {
-			command+="(" + arguments.getInt(0) + ")";
+			command += "(" + arguments.getInt(0) + ")";
 			response = solo.getCurrentTextViews(null).get(arguments.getInt(0)).getText().toString();
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -194,7 +241,7 @@ public class SoloExecutor {
 		String command = "the command  getTextViewIndex";
 		StringBuilder response = new StringBuilder();
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			List<TextView> textViews = solo.getCurrentTextViews(null);
 			for (int i = 0; i < textViews.size(); i++) {
 				if (arguments.getString(0).trim().equals(textViews.get(i).getText().toString())) {
@@ -217,7 +264,7 @@ public class SoloExecutor {
 		String command = "the command  getText";
 		String response = "";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			response = solo.getText(arguments.getInt(0)).getText().toString();
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -234,7 +281,7 @@ public class SoloExecutor {
 	private String clickOnMenuItem(JSONArray arguments) {
 		String command = "the command  clickOnMenuItem";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.clickOnMenuItem(arguments.getString(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -251,7 +298,7 @@ public class SoloExecutor {
 	private String sendKey(JSONArray arguments) {
 		String command = "the command  sendKey";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.sendKey(arguments.getInt(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -267,7 +314,7 @@ public class SoloExecutor {
 	private String clickOnView(JSONArray arguments) {
 		String command = "the command  clickOnView";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.clickOnView(solo.getCurrentViews().get((arguments.getInt(0))));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -283,7 +330,7 @@ public class SoloExecutor {
 	private String clickOnButtonWithText(JSONArray arguments) {
 		String command = "the command  clickOnButton";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.clickOnButton(arguments.getString(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -299,7 +346,7 @@ public class SoloExecutor {
 	private String clearEditText(JSONArray arguments) {
 		String command = "the command  clearEditText";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.clearEditText(arguments.getInt(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
@@ -315,13 +362,13 @@ public class SoloExecutor {
 	private String clickInList(JSONArray arguments) {
 		String command = "the command  clickInList(";
 		try {
-			command+="(" + arguments.getString(0) + ")";
+			command += "(" + arguments.getString(0) + ")";
 			solo.clickInList(arguments.getInt(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 			return error;
-		}catch (JSONException e) {
+		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 		}
@@ -332,13 +379,13 @@ public class SoloExecutor {
 	private String clickOnButton(JSONArray params) {
 		String command = "the command  clickOnButton";
 		try {
-			command +="(" + params.getString(0) + ")";
+			command += "(" + params.getString(0) + ")";
 			solo.clickOnButton(params.getInt(0));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 			return error;
-		}catch (JSONException e) {
+		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 		}
@@ -348,13 +395,13 @@ public class SoloExecutor {
 	private String enterText(JSONArray params) {
 		String command = "the command  clickOnButton";
 		try {
-			command +="(" + params.getString(0) + ")";
+			command += "(" + params.getString(0) + ")";
 			solo.enterText(params.getInt(0), params.getString(1));
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 			return error;
-		}catch (JSONException e){
+		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 		}
@@ -365,31 +412,32 @@ public class SoloExecutor {
 	private String clickOnText(JSONArray params) {
 		String command = "the command clickOnText";
 		try {
-			command+="(" + params.getString(0) + ")";
-			solo.clickOnText(params.getString(0) );
+			command += "(" + params.getString(0) + ")";
+			solo.clickOnText(params.getString(0));
 
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 			return error;
-		}catch (JSONException e){
+		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 		}
 		return SUCCESS_STRING + command;
 
 	}
-	private String clickOnHardware(JSONArray keyString){
+
+	private String clickOnHardware(JSONArray keyString) {
 		String command = "the command clickOnHardware";
 		try {
-			command+="("+keyString.getString(0)+")";
-			int key = (keyString.getString(0) == "HOME" )? KeyEvent.KEYCODE_HOME : KeyEvent.KEYCODE_BACK;
+			command += "(" + keyString.getString(0) + ")";
+			int key = (keyString.getString(0) == "HOME") ? KeyEvent.KEYCODE_HOME : KeyEvent.KEYCODE_BACK;
 			instrumentation.sendKeyDownUpSync(key);
 		} catch (Error e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 			return error;
-		}catch (JSONException e){
+		} catch (JSONException e) {
 			String error = ERROR_STRING + command + "failed due to " + e.getMessage();
 			Log.d(TAG, error);
 		}
